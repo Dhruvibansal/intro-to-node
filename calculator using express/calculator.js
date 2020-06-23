cc39e4108074be3bd0cd078c7082d8814ce7741f
Dhruvi bansal <48863167+Dhruvibansal@users.noreply.github.com>
2020-06-23 22:20:13 +0530

    Update calculator.js

--- calculator using express/calculator.js
+++ calculator using express/calculator.js
@@ -1,10 +1,23 @@
 const express = require("express");
 
-const app = express();
+const bodyParser = require("body-parser");
 
-app.get("/",function(req,res){
+const app = express();
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get("/",function(req, res){
 	res.sendFile(__dirname + "/index.html");
 	});
+
+app.post("/",function(req, res){
+
+ 	var num1= Number(req.body.num1);
+ 	var num2= Number(req.body.num2);
+ 	var result = num2 +num1;
+
+	res.send("thanks"+result);
+});
+
 app.listen(3000,function(){
 	console.log("server is running at 3000");
 });
